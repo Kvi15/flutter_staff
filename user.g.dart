083a7e77c6c1b60bde52a519df2e66dce24fd53f
Kvi15 <putslizox@gmail.com>
2024-07-10 10:02:51 +0300

    Update user.g.dart

--- user.g.dart
+++ user.g.dart
@@ -17,17 +17,20 @@ class UserAdapter extends TypeAdapter<User> {
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
     return User(
-      surname: fields[0] as String,
-      name: fields[1] as String,
-      patronymic: fields[2] as String,
-      number: fields[3] as String,
+      surname: fields[0] as String? ?? 'Фамилия',
+      name: fields[1] as String? ?? 'Имя',
+      patronymic: fields[2] as String? ?? 'Отчество',
+      number: fields[3] as String? ?? 'Номер',
+      imagePath: fields[4] as String?,
+      deviceDate: fields[5] as String? ?? '01.01.2020',
+      medicalBook: fields[6] as String? ?? 'Медкнижка',
     );
   }
 
   @override
   void write(BinaryWriter writer, User obj) {
     writer
-      ..writeByte(4)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.surname)
       ..writeByte(1)
@@ -35,16 +38,12 @@ class UserAdapter extends TypeAdapter<User> {
       ..writeByte(2)
       ..write(obj.patronymic)
       ..writeByte(3)
-      ..write(obj.number);
+      ..write(obj.number)
+      ..writeByte(4)
+      ..write(obj.imagePath)
+      ..writeByte(5)
+      ..write(obj.deviceDate)
+      ..writeByte(6)
+      ..write(obj.medicalBook);
   }
-
-  @override
-  int get hashCode => typeId.hashCode;
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is UserAdapter &&
-          runtimeType == other.runtimeType &&
-          typeId == other.typeId;
 }
