6519e2191742bc0c50b142d5c8adca8f1d9e070c
Kvi15 <putslizox@gmail.com>
2024-07-10 09:52:03 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staff/home_page/adding_a_person.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_staff/home_page/adding_a_person.dart';
 import 'package:flutter_staff/home_page/user.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
 
+  // Регистрируем адаптер User
   Hive.registerAdapter(UserAdapter());
-  await Hive.openBox<User>('usersBox');
 
+  // Открываем коробку (Box) пользователей
+  var userBox = await Hive.openBox<User>('users');
+
+  // Обновляем существующих пользователей
+  updateExistingUsers(userBox);
+
+  // Запускаем приложение
   runApp(const MyApp());
 }
 
