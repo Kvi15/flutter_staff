688833e592b2881b3809f8993d2b8bf499fbaa32
Kvi15 <putslizox@gmail.com>
2024-07-10 09:56:40 +0300

    Update user.dart

--- user.dart
+++ user.dart
@@ -5,21 +5,51 @@ part 'user.g.dart';
 @HiveType(typeId: 0)
 class User extends HiveObject {
   @HiveField(0)
-  late String surname;
+  String surname = 'Фамилия';
 
   @HiveField(1)
-  late String name;
+  String name = 'Имя';
 
   @HiveField(2)
-  late String patronymic;
+  String patronymic = 'Отчество';
 
   @HiveField(3)
-  late String number;
+  String number = 'Номер';
+
+  @HiveField(4)
+  String? imagePath;
+
+  @HiveField(5)
+  String deviceDate = '01.01.2020';
+
+  @HiveField(6)
+  String medicalBook = 'Медкнижка';
 
   User({
-    this.surname = '',
-    this.name = '',
-    this.patronymic = '',
-    this.number = '',
+    this.surname = 'Фамилия',
+    this.name = 'Имя',
+    this.patronymic = 'Отчество',
+    this.number = 'Номер',
+    this.imagePath,
+    this.deviceDate = '01.01.2020',
+    this.medicalBook = 'Медкнижка',
   });
+}
+
+void updateExistingUsers(Box<User> userBox) {
+  for (int i = 0; i < userBox.length; i++) {
+    User? user = userBox.getAt(i);
+    if (user != null) {
+      user.surname = user.surname.isNotEmpty ? user.surname : 'Фамилия';
+      user.name = user.name.isNotEmpty ? user.name : 'Имя';
+      user.patronymic =
+          user.patronymic.isNotEmpty ? user.patronymic : 'Отчество';
+      user.number = user.number.isNotEmpty ? user.number : 'Номер';
+      user.deviceDate =
+          user.deviceDate.isNotEmpty ? user.deviceDate : '01.01.2020';
+      user.medicalBook =
+          user.medicalBook.isNotEmpty ? user.medicalBook : 'Медкнижка';
+      user.save();
+    }
+  }
 }
